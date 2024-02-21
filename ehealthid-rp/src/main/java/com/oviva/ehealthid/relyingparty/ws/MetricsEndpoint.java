f5104cbe501374a15c9e1cac9bb23292d6ec7362
Thomas Richner <thomas.richner@oviva.com>
2024-02-21 15:57:31 +0100

    ARC-1299: Fix /metrics media type (#36)
    Co-authored-by: Eduard Thamm <eduard@orthopy.de>

--- ehealthid-rp/src/main/java/com/oviva/ehealthid/relyingparty/ws/MetricsEndpoint.java
+++ ehealthid-rp/src/main/java/com/oviva/ehealthid/relyingparty/ws/MetricsEndpoint.java
@@ -8,7 +8,8 @@ import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/metrics")
 public class MetricsEndpoint {
@@ -26,7 +27,8 @@ public class MetricsEndpoint {
   }
 
   @GET
-  public Response get() {
-    return Response.ok(this.registry.scrape()).build();
+  @Produces(MediaType.TEXT_PLAIN)
+  public String get() {
+    return registry.scrape();
   }
 }
