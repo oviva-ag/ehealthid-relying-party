5e75be445ff7534fcd5f23552e26196ec0748efd
Yves Weissig <yves@weissig.me>
2024-02-27 18:19:28 +0000

    Small fix for cli.sh to use Maven Wrapper

--- cli.sh
+++ cli.sh
@@ -3,6 +3,6 @@
 set -e
 
 >&2 echo "INFO compiling"
->&2 mvn --quiet clean package -DskipTests -am -pl=ehealthid-cli
+>&2 ./mvnw --quiet clean package -DskipTests -am -pl=ehealthid-cli
 >&2 echo "INFO running cli"
 java -jar ./ehealthid-cli/target/ehealthcli.jar "$@"
