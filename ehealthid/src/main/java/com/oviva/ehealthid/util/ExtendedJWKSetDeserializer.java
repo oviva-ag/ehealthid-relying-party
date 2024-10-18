c0ffeef8a2c3fce642a827dd79820bc947a45b7a
Thomas Richner <thomas.richner@oviva.com>
2024-10-18 11:17:49 +0200

    EPA-158: Improve coverage

--- ehealthid/src/main/java/com/oviva/ehealthid/util/ExtendedJWKSetDeserializer.java
+++ ehealthid/src/main/java/com/oviva/ehealthid/util/ExtendedJWKSetDeserializer.java
@@ -1,6 +1,5 @@
 package com.oviva.ehealthid.util;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -9,9 +8,7 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.nimbusds.jose.jwk.JWK;
 import com.oviva.ehealthid.fedclient.api.ExtendedJWKSet;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.List;
-import java.util.Map;
 
 public class ExtendedJWKSetDeserializer extends StdDeserializer<ExtendedJWKSet> {
 
@@ -51,25 +48,5 @@ public class ExtendedJWKSetDeserializer extends StdDeserializer<ExtendedJWKSet> 
     return jsonParser
         .getCodec()
         .readValue(node.get(fieldName).traverse(jsonParser.getCodec()), Long.class);
-  }
-
-  public static class JWKDeserializer extends StdDeserializer<JWK> {
-
-    public JWKDeserializer(Class<?> vc) {
-      super(vc);
-    }
-
-    @Override
-    public JWK deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-        throws IOException {
-
-      Map<String, Object> map =
-          jsonParser.getCodec().readValue(jsonParser, new TypeReference<Map<String, Object>>() {});
-      try {
-        return JWK.parse(map);
-      } catch (ParseException e) {
-        throw new JsonParseException(jsonParser, "failed to parse JWK", e);
-      }
-    }
   }
 }
